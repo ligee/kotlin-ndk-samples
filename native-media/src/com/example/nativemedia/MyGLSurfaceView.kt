9214eee0530b9e060c8c4a384a6b58a2bd1787a9
ligee <ilya.chernikov@jetbrains.com>
2014-12-01 18:26:54 +0100

    Ported native-media sample

---
+++ native-media/src/com/example/nativemedia/MyGLSurfaceView.kt
@@ -0,0 +1,308 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.nativemedia
+
+import android.graphics.SurfaceTexture
+import android.util.Log
+
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+import android.content.Context
+
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import android.opengl.Matrix
+
+import android.util.AttributeSet
+
+public class MyGLSurfaceView(context: Context, attributeSet: AttributeSet? = null) : GLSurfaceView(context, attributeSet) {
+
+    var mRenderer: MyRenderer? = null
+
+    {
+        init()
+    }
+
+    private fun init() {
+        setEGLContextClientVersion(2)
+        mRenderer = MyRenderer()
+        setRenderer(mRenderer)
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    public fun getSurfaceTexture(): SurfaceTexture? {
+        return mRenderer!!.surfaceTexture
+    }
+}
+
+class MyRenderer : GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
+
+    override fun onDrawFrame(glUnused: GL10) {
+        synchronized (this) {
+            if (updateSurface) {
+                surfaceTexture!!.updateTexImage()
+
+                surfaceTexture!!.getTransformMatrix(mSTMatrix)
+                updateSurface = false
+            }
+        }
+
+        // Ignore the passed-in GL10 interface, and use the GLES20
+        // class's static methods instead.
+        GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT or GLES20.GL_COLOR_BUFFER_BIT)
+        GLES20.glUseProgram(mProgram)
+        checkGlError("glUseProgram")
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, mTextureID)
+
+        mVertices.position(VERTICES_DATA_POS_OFFSET)
+        GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false, VERTICES_DATA_STRIDE_BYTES, mVertices)
+        checkGlError("glVertexAttribPointer maPosition")
+        GLES20.glEnableVertexAttribArray(maPositionHandle)
+        checkGlError("glEnableVertexAttribArray maPositionHandle")
+
+        mVertices.position(VERTICES_DATA_UV_OFFSET)
+        GLES20.glVertexAttribPointer(maTextureHandle, 3, GLES20.GL_FLOAT, false, VERTICES_DATA_STRIDE_BYTES, mVertices)
+        checkGlError("glVertexAttribPointer maTextureHandle")
+        GLES20.glEnableVertexAttribArray(maTextureHandle)
+        checkGlError("glEnableVertexAttribArray maTextureHandle")
+
+        Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, mMMatrix, 0)
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0)
+
+        GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0)
+        GLES20.glUniformMatrix4fv(muSTMatrixHandle, 1, false, mSTMatrix, 0)
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+        checkGlError("glDrawArrays")
+    }
+
+    override fun onSurfaceChanged(glUnused: GL10, width: Int, height: Int) {
+        // Ignore the passed-in GL10 interface, and use the GLES20
+        // class's static methods instead.
+        GLES20.glViewport(0, 0, width, height)
+        mRatio = width.toFloat() / height.toFloat()
+        Matrix.frustumM(mProjMatrix, 0, -mRatio, mRatio, (-1).toFloat(), 1f, 3f, 7f)
+    }
+
+    override fun onSurfaceCreated(glUnused: GL10, config: EGLConfig) {
+        // Ignore the passed-in GL10 interface, and use the GLES20
+        // class's static methods instead.
+
+        /* Set up alpha blending and an Android background color */
+        GLES20.glEnable(GLES20.GL_BLEND)
+        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA)
+        GLES20.glClearColor(0.643.toFloat(), 0.776.toFloat(), 0.223.toFloat(), 1.0.toFloat())
+
+        /* Set up shaders and handles to their variables */
+        mProgram = createProgram(mVertexShader, mFragmentShader)
+        if (mProgram == 0) {
+            return
+        }
+        maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition")
+        checkGlError("glGetAttribLocation aPosition")
+        if (maPositionHandle == -1) {
+            throw RuntimeException("Could not get attrib location for aPosition")
+        }
+        maTextureHandle = GLES20.glGetAttribLocation(mProgram, "aTextureCoord")
+        checkGlError("glGetAttribLocation aTextureCoord")
+        if (maTextureHandle == -1) {
+            throw RuntimeException("Could not get attrib location for aTextureCoord")
+        }
+
+        muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix")
+        checkGlError("glGetUniformLocation uMVPMatrix")
+        if (muMVPMatrixHandle == -1) {
+            throw RuntimeException("Could not get attrib location for uMVPMatrix")
+        }
+
+        muSTMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uSTMatrix")
+        checkGlError("glGetUniformLocation uSTMatrix")
+        if (muMVPMatrixHandle == -1) {
+            throw RuntimeException("Could not get attrib location for uSTMatrix")
+        }
+
+        checkGlError("glGetUniformLocation uCRatio")
+        if (muMVPMatrixHandle == -1) {
+            throw RuntimeException("Could not get attrib location for uCRatio")
+        }
+
+        /*
+         * Create our texture. This has to be done each time the
+         * surface is created.
+         */
+
+        val textures = IntArray(1)
+        GLES20.glGenTextures(1, textures, 0)
+
+        mTextureID = textures[0]
+        GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, mTextureID)
+        checkGlError("glBindTexture mTextureID")
+
+        // Can't do mipmapping with camera source
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST.toFloat())
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR.toFloat())
+        // Clamp to edge is the only option
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+        checkGlError("glTexParameteri mTextureID")
+
+        /*
+         * Create the SurfaceTexture that will feed this textureID, and pass it to the camera
+         */
+
+        surfaceTexture = SurfaceTexture(mTextureID)
+        surfaceTexture!!.setOnFrameAvailableListener(this)
+
+        Matrix.setLookAtM(mVMatrix, 0, 0f, 0f, 4.toFloat(), 0.toFloat(), 0.toFloat(), 0.toFloat(), 0.toFloat(), 1.0.toFloat(), 0.0.toFloat())
+
+        synchronized (this) {
+            updateSurface = false
+        }
+    }
+
+    synchronized override fun onFrameAvailable(surface: SurfaceTexture) {
+        /* For simplicity, SurfaceTexture calls here when it has new
+         * data available.  Call may come in from some random thread,
+         * so let's be safe and use synchronize. No OpenGL calls can be done here.
+         */
+        updateSurface = true
+        //Log.v(TAG, "onFrameAvailable " + surface.getTimestamp());
+    }
+
+    private fun loadShader(shaderType: Int, source: String): Int {
+        var shader = GLES20.glCreateShader(shaderType)
+        if (shader != 0) {
+            GLES20.glShaderSource(shader, source)
+            GLES20.glCompileShader(shader)
+            val compiled = IntArray(1)
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0)
+            if (compiled[0] == 0) {
+                Log.e(TAG, "Could not compile shader " + shaderType + ":")
+                Log.e(TAG, GLES20.glGetShaderInfoLog(shader))
+                GLES20.glDeleteShader(shader)
+                shader = 0
+            }
+        }
+        return shader
+    }
+
+    private fun createProgram(vertexSource: String, fragmentSource: String): Int {
+        val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource)
+        if (vertexShader == 0) {
+            return 0
+        }
+        val pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource)
+        if (pixelShader == 0) {
+            return 0
+        }
+
+        var program = GLES20.glCreateProgram()
+        if (program != 0) {
+            GLES20.glAttachShader(program, vertexShader)
+            checkGlError("glAttachShader")
+            GLES20.glAttachShader(program, pixelShader)
+            checkGlError("glAttachShader")
+            GLES20.glLinkProgram(program)
+            val linkStatus = IntArray(1)
+            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0)
+            if (linkStatus[0] != GLES20.GL_TRUE) {
+                Log.e(TAG, "Could not link program: ")
+                Log.e(TAG, GLES20.glGetProgramInfoLog(program))
+                GLES20.glDeleteProgram(program)
+                program = 0
+            }
+        }
+        return program
+    }
+
+    private fun checkGlError(op: String) {
+        var error: Int
+        while (true) {
+            error = GLES20.glGetError()
+            if (error == GLES20.GL_NO_ERROR) break
+            Log.e(TAG, op + ": glError " + error)
+            throw RuntimeException(op + ": glError " + error)
+        }
+    }
+
+    private val mVerticesData = floatArray(
+        // X, Y, Z, U, V
+        -1.0f, -1.0f, 0f, 0f, 0f,
+         1.0f, -1.0f, 0f, 1f, 0f,
+        -1.0f,  1.0f, 0f, 0f, 1f,
+         1.0f,  1.0f, 0f, 1f, 1f )
+
+    private val mVertices: FloatBuffer
+
+    private val mVertexShader = "uniform mat4 uMVPMatrix;\n" + "uniform mat4 uSTMatrix;\n" + "attribute vec4 aPosition;\n" + "attribute vec4 aTextureCoord;\n" + "varying vec2 vTextureCoord;\n" + "void main() {\n" + "  gl_Position = uMVPMatrix * aPosition;\n" + "  vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" + "}\n"
+
+    private val mFragmentShader = "#extension GL_OES_EGL_image_external : require\n" + "precision mediump float;\n" + "varying vec2 vTextureCoord;\n" + "uniform samplerExternalOES sTexture;\n" + "void main() {\n" + "  gl_FragColor = texture2D(sTexture, vTextureCoord);\n" + "}\n"
+
+    private val mMVPMatrix = FloatArray(16)
+    private val mProjMatrix = FloatArray(16)
+    private val mMMatrix = FloatArray(16)
+    private val mVMatrix = FloatArray(16)
+    private val mSTMatrix = FloatArray(16)
+
+    private var mProgram: Int = 0
+    private var mTextureID: Int = 0
+    private var muMVPMatrixHandle: Int = 0
+    private var muSTMatrixHandle: Int = 0
+    private var maPositionHandle: Int = 0
+    private var maTextureHandle: Int = 0
+
+    private var mRatio = 1.0.toFloat()
+    public var surfaceTexture: SurfaceTexture? = null
+        private set
+    private var updateSurface = false
+
+    {
+        mVertices = ByteBuffer.allocateDirect(mVerticesData.count() * FLOAT_SIZE_BYTES).order(ByteOrder.nativeOrder()).asFloatBuffer()
+        mVertices.put(mVerticesData).position(0)
+
+        Matrix.setIdentityM(mSTMatrix, 0)
+        Matrix.setIdentityM(mMMatrix, 0)
+        Matrix.rotateM(mMMatrix, 0, 20f, 0f, 1f, 0f)
+    }
+
+    class object {
+
+        private val FLOAT_SIZE_BYTES = 4
+        private val VERTICES_DATA_STRIDE_BYTES = 5 * FLOAT_SIZE_BYTES
+        private val VERTICES_DATA_POS_OFFSET = 0
+        private val VERTICES_DATA_UV_OFFSET = 3
+
+        private val TAG = "MyRenderer"
+
+        // Magic key
+        private val GL_TEXTURE_EXTERNAL_OES = 36197
+    }
+}
---
+++ native-media/src/com/example/nativemedia/NativeMedia.kt
@@ -0,0 +1,395 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.nativemedia
+
+import android.app.Activity
+import android.graphics.SurfaceTexture
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.util.Log
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import java.io.IOException
+import kotlin.platform.platformStatic
+import android.widget.Adapter
+
+public class NativeMedia : Activity() {
+
+    var mSourceString: String? = null
+    var mSinkString: String? = null
+
+    // member variables for Java media player
+    var mMediaPlayer: MediaPlayer? = null
+    var mMediaPlayerIsPrepared = false
+    var mSurfaceView1: SurfaceView? = null
+    var mSurfaceHolder1: SurfaceHolder? = null
+
+    // member variables for native media player
+    var mIsPlayingStreaming = false
+    var mSurfaceView2: SurfaceView? = null
+    var mSurfaceHolder2: SurfaceHolder? = null
+
+    var mSelectedVideoSink: VideoSink? = null
+    var mJavaMediaPlayerVideoSink: VideoSink? = null
+    var mNativeMediaPlayerVideoSink: VideoSink? = null
+
+    var mSurfaceHolder1VideoSink: SurfaceHolderVideoSink? = null
+    var mSurfaceHolder2VideoSink: SurfaceHolderVideoSink? = null
+    var mGLView1VideoSink: GLViewVideoSink? = null
+    var mGLView2VideoSink: GLViewVideoSink? = null
+
+    /** Called when the activity is first created. */
+    override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
+        setContentView(R.layout.main)
+
+        mGLView1 = findViewById(R.id.glsurfaceview1) as MyGLSurfaceView
+        mGLView2 = findViewById(R.id.glsurfaceview2) as MyGLSurfaceView
+
+        // initialize native media system
+        createEngine()
+
+        // set up the Surface 1 video sink
+        mSurfaceView1 = findViewById(R.id.surfaceview1) as SurfaceView
+        mSurfaceHolder1 = mSurfaceView1!!.getHolder()
+
+        mSurfaceHolder1!!.addCallback(object : SurfaceHolder.Callback {
+
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+                Log.v(TAG, "surfaceChanged format=" + format + ", width=" + width + ", height=" + height)
+            }
+
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                Log.v(TAG, "surfaceCreated")
+                setSurface(holder.getSurface())
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                Log.v(TAG, "surfaceDestroyed")
+            }
+
+        })
+
+        // set up the Surface 2 video sink
+        mSurfaceView2 = findViewById(R.id.surfaceview2) as SurfaceView
+        mSurfaceHolder2 = mSurfaceView2!!.getHolder()
+
+        mSurfaceHolder2!!.addCallback(object : SurfaceHolder.Callback {
+
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+                Log.v(TAG, "surfaceChanged format=" + format + ", width=" + width + ", height=" + height)
+            }
+
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                Log.v(TAG, "surfaceCreated")
+                setSurface(holder.getSurface())
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                Log.v(TAG, "surfaceDestroyed")
+            }
+
+        })
+
+        // create Java media player
+        mMediaPlayer = MediaPlayer()
+
+        // set up Java media player listeners
+        mMediaPlayer!!.setOnPreparedListener(object : MediaPlayer.OnPreparedListener {
+
+            override fun onPrepared(mediaPlayer: MediaPlayer) {
+                val width = mediaPlayer.getVideoWidth()
+                val height = mediaPlayer.getVideoHeight()
+                Log.v(TAG, "onPrepared width=" + width + ", height=" + height)
+                if (width != 0 && height != 0 && mJavaMediaPlayerVideoSink != null) {
+                    mJavaMediaPlayerVideoSink!!.setFixedSize(width, height)
+                }
+                mMediaPlayerIsPrepared = true
+                mediaPlayer.start()
+            }
+
+        })
+
+        mMediaPlayer!!.setOnVideoSizeChangedListener(object : MediaPlayer.OnVideoSizeChangedListener {
+
+            override fun onVideoSizeChanged(mediaPlayer: MediaPlayer, width: Int, height: Int) {
+                Log.v(TAG, "onVideoSizeChanged width=" + width + ", height=" + height)
+                if (width != 0 && height != 0 && mJavaMediaPlayerVideoSink != null) {
+                    mJavaMediaPlayerVideoSink!!.setFixedSize(width, height)
+                }
+            }
+
+        })
+
+        // initialize content source spinner
+        val sourceSpinner = findViewById(R.id.source_spinner) as Spinner
+        val sourceAdapter = ArrayAdapter.createFromResource(this, R.array.source_array, android.R.layout.simple_spinner_item)
+        sourceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sourceSpinner.setAdapter(sourceAdapter)
+        sourceSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+                mSourceString = parent.getItemAtPosition(pos).toString()
+                Log.v(TAG, "onItemSelected " + mSourceString)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<out Adapter>?) {
+                Log.v(TAG, "onNothingSelected")
+                mSourceString = null
+            }
+
+        })
+
+        // initialize video sink spinner
+        val sinkSpinner = findViewById(R.id.sink_spinner) as Spinner
+        val sinkAdapter = ArrayAdapter.createFromResource(this, R.array.sink_array, android.R.layout.simple_spinner_item)
+        sinkAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sinkSpinner.setAdapter(sinkAdapter)
+        sinkSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+                mSinkString = parent.getItemAtPosition(pos).toString()
+                Log.v(TAG, "onItemSelected " + mSinkString)
+                if ("Surface 1" == mSinkString) {
+                    if (mSurfaceHolder1VideoSink == null) {
+                        mSurfaceHolder1VideoSink = SurfaceHolderVideoSink(mSurfaceHolder1!!)
+                    }
+                    mSelectedVideoSink = mSurfaceHolder1VideoSink
+                } else if ("Surface 2" == mSinkString) {
+                    if (mSurfaceHolder2VideoSink == null) {
+                        mSurfaceHolder2VideoSink = SurfaceHolderVideoSink(mSurfaceHolder2!!)
+                    }
+                    mSelectedVideoSink = mSurfaceHolder2VideoSink
+                } else if ("SurfaceTexture 1" == mSinkString) {
+                    if (mGLView1VideoSink == null) {
+                        mGLView1VideoSink = GLViewVideoSink(mGLView1!!)
+                    }
+                    mSelectedVideoSink = mGLView1VideoSink
+                } else if ("SurfaceTexture 2" == mSinkString) {
+                    if (mGLView2VideoSink == null) {
+                        mGLView2VideoSink = GLViewVideoSink(mGLView2!!)
+                    }
+                    mSelectedVideoSink = mGLView2VideoSink
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<out Adapter>?) {
+                Log.v(TAG, "onNothingSelected")
+                mSinkString = null
+                mSelectedVideoSink = null
+            }
+
+        })
+
+        // initialize button click handlers
+
+        // Java MediaPlayer start/pause
+
+        (findViewById(R.id.start_java) as Button).setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(view: View) {
+                if (mJavaMediaPlayerVideoSink == null) {
+                    if (mSelectedVideoSink == null) {
+                        return
+                    }
+                    mSelectedVideoSink!!.useAsSinkForJava(mMediaPlayer!!)
+                    mJavaMediaPlayerVideoSink = mSelectedVideoSink
+                }
+                if (!mMediaPlayerIsPrepared) {
+                    if (mSourceString != null) {
+                        try {
+                            mMediaPlayer!!.setDataSource(mSourceString)
+                        } catch (e: IOException) {
+                            Log.e(TAG, "IOException " + e)
+                        }
+
+                        mMediaPlayer!!.prepareAsync()
+                    }
+                } else if (mMediaPlayer!!.isPlaying()) {
+                    mMediaPlayer!!.pause()
+                } else {
+                    mMediaPlayer!!.start()
+                }
+            }
+
+        })
+
+        // native MediaPlayer start/pause
+
+        (findViewById(R.id.start_native) as Button).setOnClickListener(object : View.OnClickListener {
+
+            var created = false
+            override fun onClick(view: View) {
+                if (!created) {
+                    if (mNativeMediaPlayerVideoSink == null) {
+                        if (mSelectedVideoSink == null) {
+                            return
+                        }
+                        mSelectedVideoSink!!.useAsSinkForNative()
+                        mNativeMediaPlayerVideoSink = mSelectedVideoSink
+                    }
+                    if (mSourceString != null) {
+                        created = createStreamingMediaPlayer(mSourceString!!)
+                    }
+                }
+                if (created) {
+                    mIsPlayingStreaming = !mIsPlayingStreaming
+                    setPlayingStreamingMediaPlayer(mIsPlayingStreaming)
+                }
+            }
+
+        })
+
+        // finish
+
+        (findViewById(R.id.finish) as Button).setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(view: View) {
+                finish()
+            }
+
+        })
+
+        // Java MediaPlayer rewind
+
+        (findViewById(R.id.rewind_java) as Button).setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(view: View) {
+                if (mMediaPlayerIsPrepared) {
+                    mMediaPlayer!!.seekTo(0)
+                }
+            }
+
+        })
+
+        // native MediaPlayer rewind
+
+        (findViewById(R.id.rewind_native) as Button).setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(view: View) {
+                if (mNativeMediaPlayerVideoSink != null) {
+                    rewindStreamingMediaPlayer()
+                }
+            }
+
+        })
+
+    }
+
+    /** Called when the activity is about to be paused. */
+    override fun onPause() {
+        mIsPlayingStreaming = false
+        setPlayingStreamingMediaPlayer(false)
+        mGLView1!!.onPause()
+        mGLView2!!.onPause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mGLView1!!.onResume()
+        mGLView2!!.onResume()
+    }
+
+    /** Called when the activity is about to be destroyed. */
+    override fun onDestroy() {
+        shutdown()
+        super.onDestroy()
+    }
+
+    private var mGLView1: MyGLSurfaceView? = null
+    private var mGLView2: MyGLSurfaceView? = null
+
+    // VideoSink abstracts out the difference between Surface and SurfaceTexture
+    // aka SurfaceHolder and GLSurfaceView
+    abstract class VideoSink {
+
+        abstract fun setFixedSize(width: Int, height: Int)
+        abstract fun useAsSinkForJava(mediaPlayer: MediaPlayer)
+        abstract fun useAsSinkForNative()
+
+    }
+
+    class SurfaceHolderVideoSink(private val mSurfaceHolder: SurfaceHolder) : VideoSink() {
+
+        override fun setFixedSize(width: Int, height: Int) {
+            mSurfaceHolder.setFixedSize(width, height)
+        }
+
+        override fun useAsSinkForJava(mediaPlayer: MediaPlayer) {
+            // Use the newer MediaPlayer.setSurface(Surface) since API level 14
+            // instead of MediaPlayer.setDisplay(mSurfaceHolder) since API level 1,
+            // because setSurface also works with a Surface derived from a SurfaceTexture.
+            val s = mSurfaceHolder.getSurface()
+            mediaPlayer.setSurface(s)
+            s.release()
+        }
+
+        override fun useAsSinkForNative() {
+            val s = mSurfaceHolder.getSurface()
+            setSurface(s)
+            s.release()
+        }
+
+    }
+
+    class GLViewVideoSink(private val mMyGLSurfaceView: MyGLSurfaceView) : VideoSink() {
+
+        override fun setFixedSize(width: Int, height: Int) {
+        }
+
+        override fun useAsSinkForJava(mediaPlayer: MediaPlayer) {
+            val st = mMyGLSurfaceView.getSurfaceTexture()
+            val s = Surface(st)
+            mediaPlayer.setSurface(s)
+            s.release()
+        }
+
+        override fun useAsSinkForNative() {
+            val st = mMyGLSurfaceView.getSurfaceTexture()
+            val s = Surface(st)
+            setSurface(s)
+            s.release()
+        }
+
+    }
+
+    class object {
+        val TAG = "NativeMedia"
+
+        /** Native methods, implemented in jni folder */
+        public native platformStatic fun createEngine(): Unit = null!!
+
+        public native platformStatic fun createStreamingMediaPlayer(filename: String): Boolean = null!!
+        public native platformStatic fun setPlayingStreamingMediaPlayer(isPlaying: Boolean): Unit = null!!
+        public native platformStatic fun shutdown(): Unit = null!!
+        public native platformStatic fun setSurface(surface: Surface): Unit = null!!
+        public native platformStatic fun rewindStreamingMediaPlayer(): Unit = null!!
+
+                /** Load jni .so on initialization */
+        {
+            System.loadLibrary("native-media-jni")
+        }
+    }
+
+}
